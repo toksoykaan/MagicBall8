2799e92b49af96260a9d61781e541f849e381d49
toksoykaan <toksoykaan@gmail.com>
2022-08-11 18:46:20 +0300

    Completed learning challange, a ball answers numerous questions with 5 answers inside a ball. Enjoy

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,44 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          backgroundColor: Colors.lightBlueAccent,
+          appBar: AppBar(
+            title: Text('Ask Me Anything'),
+            backgroundColor: Colors.lightBlueAccent,
+          ),
+          body: BallPage(),
+        ),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BallPageStateful();
+  }
+}
+
+class BallPageStateful extends StatefulWidget {
+  @override
+  State<BallPageStateful> createState() => _BallPageStatefulState();
+}
+
+class _BallPageStatefulState extends State<BallPageStateful> {
+  int ballImageNumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextButton(
+        onPressed: () {
+          setState(() {
+            ballImageNumber = Random().nextInt(5) + 1;
+          });
+        },
+        child: Image.asset('images/ball$ballImageNumber.png'),
+      ),
+    );
+  }
+}
